0b733a9c1f5270e852d18a5bd385bcf1e7b64e47
Scolonel <sergikss@gmail.com>
2026-08-14 10:35:56 +0300

    v605g(136) -     Делаем  перезагрузку устройства после получения команды     restяяя, после программирования индикатора

--- User/Src/modes.c
+++ User/Src/modes.c
@@ -7764,12 +7764,14 @@ void UploadFW_Nextion(void) // обновление индикатора NEXTION
     //  //CreatDelay(5000000);
     //  SetModeDevice (MODEMENU); // принудительная установка режима прибора
     //  CmdInitPage(1);
+    myBeep(500);
+    HAL_Delay(500);
+    //NVIC_SystemReset(); //  уходим в сброс
     CmdInitPage(0);// вызов окна заставки
-    HAL_Delay(100);
-    SetMode (ModeWelcome);
+    HAL_Delay(100);// индикатор после сброса, время не понятно!
     CmdInitPage(0);// посылка команды переключения окна на Welcome и установка признака первого входа
-    HAL_Delay(500);// индикатор после сброса, время не понятно!
-    myBeep(125);
+    TimeBegin = HAL_GetTick();
+    SetMode (ModeWelcome);
   }
   
 }
